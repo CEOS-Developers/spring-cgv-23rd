d5c8b1cfefdc2efa897275efa3e1d835c28ea765
dongwook <boogiewooki02@gmail.com>
2026-03-19 00:31:36 +0900

    feat: Schedule 엔티티 구현

--- src/main/java/com/cgv/spring_boot/domain/schedule/entity/Schedule.java
+++ src/main/java/com/cgv/spring_boot/domain/schedule/entity/Schedule.java
@@ -1,4 +1,40 @@
 package com.cgv.spring_boot.domain.schedule.entity;
 
-public class Schedule {
+import com.cgv.spring_boot.domain.movie.entity.Movie;
+import com.cgv.spring_boot.domain.theater.entity.Hall;
+import com.cgv.spring_boot.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class Schedule extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "schedule_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime startTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hall_id")
+    private Hall hall;
+
+    @Builder
+    public Schedule(LocalDateTime startTime, Movie movie, Hall hall) {
+        this.startTime = startTime;
+        this.movie = movie;
+        this.hall = hall;
+    }
 }
