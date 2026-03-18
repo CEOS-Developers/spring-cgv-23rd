61d82c3306efdf89a92beea21ca1d5ee4e29f3e7
dongwook <boogiewooki02@gmail.com>
2026-03-18 17:14:35 +0900

    feat: MovieController 구현

---
+++ src/main/java/com/cgv/spring_boot/domain/movie/controller/MovieController.java
@@ -0,0 +1,39 @@
+package com.cgv.spring_boot.domain.movie.controller;
+
+import com.cgv.spring_boot.domain.movie.dto.request.MovieCreateRequest;
+import com.cgv.spring_boot.domain.movie.dto.response.MovieResponse;
+import com.cgv.spring_boot.domain.movie.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/movies")
+@RequiredArgsConstructor
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @PostMapping("/")
+    public ResponseEntity<Long> createMovie(@RequestBody MovieCreateRequest request) {
+        return ResponseEntity.ok(movieService.saveMovie(request));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<MovieResponse>> getAllMovies() {
+        return ResponseEntity.ok(movieService.findAllMovies());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieResponse> getMovie(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(movieService.findMovieById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable("id") Long id) {
+        movieService.deleteMovieById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
