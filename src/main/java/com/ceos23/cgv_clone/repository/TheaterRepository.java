3dba5361fd190df4d53d9c639244ab817d07ee26
Jonghun Lim <jhlim00812@gmail.com>
2026-03-19 22:24:11 +0900

    [feat] TheaterResponse DTO 및 TheaterService 추가, TheaterFavoriteRepository 및 TheaterRepository 수정

--- src/main/java/com/ceos23/cgv_clone/repository/TheaterRepository.java
+++ src/main/java/com/ceos23/cgv_clone/repository/TheaterRepository.java
@@ -3,5 +3,8 @@ package com.ceos23.cgv_clone.repository;
 import com.ceos23.cgv_clone.domain.theater.Theater;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TheaterRepository extends JpaRepository<Theater, Long> {
+    List<Theater> findAllByRegion(String region);
 }
