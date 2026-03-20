4b83b79f45d95d6414e753b73fe1d3c9f8ed428e
Junseok Ahn <rrvvy9595@gmail.com>
2026-03-20 16:10:26 +0900

    Initial Commit

---
+++ src/test/java/com/ceos/spring_cgv_23rd/SpringCgv23rdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceos.spring_cgv_23rd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCgv23rdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
