41821a0b795b11e6bad84e4cbdb42da3c8e09dc1
dongwook <boogiewooki02@gmail.com>
2026-03-25 20:47:40 +0900

    feat: Swagger 권한인증 추가

--- src/main/java/com/cgv/spring_boot/global/config/SwaggerConfig.java
+++ src/main/java/com/cgv/spring_boot/global/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.cgv.spring_boot.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,11 +13,20 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("CGV API Document")
                         .description("CGV API 명세서")
-                        .version("1.0.0")
-                );
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
