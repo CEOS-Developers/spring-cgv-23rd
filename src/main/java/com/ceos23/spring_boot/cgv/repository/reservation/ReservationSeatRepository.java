de0d9906a93e095242ae2f4b5045672b23d18974
Oh-Jisong <dhwlthd4725@gmail.com>
2026-03-27 01:42:48 +0900

    [Fix] 예매 좌석 중복 체크 시 예약 상태 조건 추가

--- src/main/java/com/ceos23/spring_boot/cgv/repository/reservation/ReservationSeatRepository.java
+++ src/main/java/com/ceos23/spring_boot/cgv/repository/reservation/ReservationSeatRepository.java
@@ -4,12 +4,17 @@ import com.ceos23.spring_boot.cgv.domain.cinema.SeatTemplate;
 import com.ceos23.spring_boot.cgv.domain.movie.Screening;
 import com.ceos23.spring_boot.cgv.domain.reservation.Reservation;
 import com.ceos23.spring_boot.cgv.domain.reservation.ReservationSeat;
+import com.ceos23.spring_boot.cgv.domain.reservation.ReservationStatus;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationSeatRepository extends JpaRepository<ReservationSeat, Long> {
 
-    boolean existsByScreeningAndSeatTemplate(Screening screening, SeatTemplate seatTemplate);
+    boolean existsByScreeningAndSeatTemplateAndReservation_Status(
+            Screening screening,
+            SeatTemplate seatTemplate,
+            ReservationStatus status
+    );
 
     List<ReservationSeat> findAllByReservation(Reservation reservation);
 }
