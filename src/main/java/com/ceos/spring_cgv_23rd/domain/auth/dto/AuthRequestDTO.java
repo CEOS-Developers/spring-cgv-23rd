e7722baa2d110ff44e5e2d811927a769b8fc45ae
Junseok Ahn <rrvvy9595@gmail.com>
2026-03-28 14:58:18 +0900

    [FEAT] 회원가입/로그인 요청 dto 유효성 검증 로직 상세화

--- src/main/java/com/ceos/spring_cgv_23rd/domain/auth/dto/AuthRequestDTO.java
+++ src/main/java/com/ceos/spring_cgv_23rd/domain/auth/dto/AuthRequestDTO.java
@@ -1,10 +1,8 @@
 package com.ceos.spring_cgv_23rd.domain.auth.dto;
 
 import com.ceos.spring_cgv_23rd.domain.user.enums.Gender;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 
 import java.time.LocalDate;
@@ -13,37 +11,52 @@ public class AuthRequestDTO {
 
     @Builder
     public record SignupRequestDTO(
-            @NotBlank @Size(max = 20)
+            @NotBlank(message = "아이디가 없습니다.")
+            @Size(max = 20, message = "아이디는 20자 이하로 입력해주세요.")
             String username,
 
-            @NotBlank @Size(min = 8, max = 20)
+            @NotBlank(message = "비밀번호가 없습니다.")
+            @Pattern(
+                    regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$",
+                    message = "비밀번호는 8~16자이며, 영문·숫자·특수문자를 모두 포함해야 합니다."
+            )
             String password,
 
-            @NotBlank @Size(max = 20)
+            @NotBlank(message = "이름이 없습니다.")
+            @Size(max = 20, message = "이름은 20자 이하로 입력해주세요.")
             String name,
 
-            @NotBlank @Email @Size(max = 50)
+            @NotBlank(message = "이메일이 없습니다.")
+            @Email(message = "이메일 형식이 올바르지 않습니다.")
+            @Size(max = 50, message = "이메일은 50자 이하로 입력해주세요.")
             String email,
 
-            @NotBlank @Size(max = 20)
+            @NotBlank(message = "전화번호가 없습니다.")
+            @Pattern(
+                    regexp = "^010\\d{7,8}$",
+                    message = "전화번호 형식이 올바르지 않습니다."
+            )
             String phone,
 
+            @NotNull(message = "생년월일이 없습니다.")
+            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
             LocalDate birth,
 
-            @NotBlank @Size(max = 20)
+            @NotBlank(message = "닉네임이 없습니다.")
+            @Size(min = 2, max = 20, message = "닉네임은 2자 이상 20자 이하로 입력해주세요.")
             String nickname,
 
-            @NotNull
+            @NotNull(message = "성별이 없습니다.")
             Gender gender
     ) {
     }
 
     @Builder
     public record LoginRequestDTO(
-            @NotBlank
+            @NotBlank(message = "아이디가 없습니다.")
             String username,
 
-            @NotBlank
+            @NotBlank(message = "비밀번호가 없습니다.")
             String password
     ) {
     }
