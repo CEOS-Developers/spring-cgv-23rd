a2517269c707837c6db8029e25997b5b85a02445
Jonghun Lim <jhlim00812@gmail.com>
2026-05-02 02:01:41 +0900

    [refactor] TheaterService 및 ScheduleService 리팩토링, 중복 코드 제거 및 메서드 구조 개선

--- src/main/java/com/ceos23/cgv_clone/theater/repository/ScheduleRepository.java
+++ src/main/java/com/ceos23/cgv_clone/theater/repository/ScheduleRepository.java
@@ -8,8 +8,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-    List<Schedule> findAllByMovie_IdAndScreen_Theater_Id(Long movieId, Long screenTheaterId);
-
     @Query("""
             SELECT s
             FROM Schedule s
@@ -17,6 +15,17 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
             JOIN FETCH s.screen sc
             JOIN FETCH sc.screenType
             where s.id = :scheduleId
-    """)
+            """)
     Optional<Schedule> findByIdWithMovieAndScreenType(Long scheduleId);
+
+    @Query("""
+            SELECT s
+            FROM Schedule s
+            JOIN FETCH s.movie
+            JOIN FETCH s.screen sc
+            JOIN FETCH sc.theater
+            WHERE s.movie.id = :movieId
+                AND sc.theater.id = :theaterId
+            """)
+    List<Schedule> findAllByMovieIdAndScreenTheaterId(Long movieId, Long theaterId);
 }
