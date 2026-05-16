c7e320ecedbb265b645761cfa817f10882ebc03e
gitaehee <rainwifekth@naver.com>
2026-05-16 19:58:57 +0900

    feat: 캐시 도입

--- spring-boot/src/main/java/com/ceos23/spring_boot/Application.java
+++ spring-boot/src/main/java/com/ceos23/spring_boot/Application.java
@@ -2,7 +2,9 @@ package com.ceos23.spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class Application {
 
